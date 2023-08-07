b7d3ffe1a728ecae2ca465a4e4c74729638d6934
GabrielLisandro <141675470+GabrielLisandro@users.noreply.github.com>
2023-08-07 16:40:56 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicio7guia5/Matriz.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio7guia5;
+
+/**
+ *
+ * @author glk05
+ */
+public class Matriz {
+   private boolean coincideSubmatriz(int[][] matrizM, int[][] matrizP, int inicioFila, int inicioColumna) {
+       for (int i = 0; i < matrizP; i++) {
+           for (int j = 0; j < matrizP; j++) {
+               if (true) {
+                   
+               }
+           }
+       }
+    
+    
+    
+}
